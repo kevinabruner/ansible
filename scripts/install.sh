c74ea63758c626b4d9e09e804e1bd6339545cc49
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-08 22:27:00 -0400

    adding machines

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,6 @@
 gitDir="/home/kevin/ansible"
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
-sudo apt-get install ansible -y
+sudo apt-get install python3-pip ansible -y
+#python3 -m pip install --user kevin
 sudo cp -R $gitDir/ansible-files/* /etc/ansible
