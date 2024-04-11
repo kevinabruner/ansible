b17d1946985ea120538ab910934a841bc1493660
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-10 20:59:17 -0400

    moving hosts declaration

--- scripts/get_ansible_info.py
+++ scripts/get_ansible_info.py
@@ -57,13 +57,15 @@ for repo in repos:
                     
     #iterates through the vms 
     for vm in vms["results"]:    
+
         indent_level = 1
+        write_with_indent(indent_level, "hosts:")      
+
         if (
             vm['custom_fields']['VMorContainer'][0] == "vm" and
             vm['custom_fields']['repos'] == repo and
             vm['status']['value'] == 'active'
         ):      
-                write_with_indent(indent_level, "hosts:")      
                 write_with_indent(indent_level + 1, vm["name"] + ":")
                 write_with_indent(indent_level + 2, "ansible host: " + vm["primary_ip4"]["address"].split("/")[0])
 
