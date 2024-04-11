0a36dae22b4f5e07083ed29a6dcaa2572bddd9fb
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-04-10 21:00:29 -0400

    oops

--- scripts/get_ansible_info.py
+++ scripts/get_ansible_info.py
@@ -53,13 +53,13 @@ shutil.copy(gitDir + '/inventory.template', gitDir + '/inventory.yaml')
 for repo in repos:
     indent_level = 0
     
-    write_with_indent(indent_level, repo + ":")    
+    write_with_indent(indent_level, repo + ":")   
+    write_with_indent(1, "hosts:")      
                     
     #iterates through the vms 
     for vm in vms["results"]:    
 
         indent_level = 1
-        write_with_indent(indent_level, "hosts:")      
 
         if (
             vm['custom_fields']['VMorContainer'][0] == "vm" and
